8745b2ff99de258db123856679329c1d24dab017
Jorge I Tabares H <jorge.tabares@itbservices.com.co>
2020-09-29 09:14:54 -0500

    Bug fixed in DB transaction

--- infrastucture/commands/DBTransaction/rollbackTransaction.js
+++ infrastucture/commands/DBTransaction/rollbackTransaction.js
@@ -1,4 +1,4 @@
-const pool = require("../postgresDB");
+const pool = require("../../postgresDB");
 const sqlQueries = require("../../sqlQueries/sqlQueriesDBTransaction");
 
 const rollbackTransacion = async () => {
